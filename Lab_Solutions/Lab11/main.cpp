db475bdba15f35e3bb1c57df529e56e38da079ab
martinhundrup <hundrupm@gmail.com>
2025-04-01 17:25:47 -0700

    updated lab 11 main

--- Lab_Solutions/Lab11/main.cpp
+++ Lab_Solutions/Lab11/main.cpp
@@ -10,23 +10,10 @@
 #include "Interpreter.hpp"
 #include "Sorting.hpp"
 
-long long int foo(int n, int x) {
-
-	if (x == 0) return 1;
-	long long int acc = 0;
-
-	for (int i = 0; i < n; i++) {
-
-		acc += foo(n, x - 1);
-	}
-
-	return acc;
-}
-
 int main() {
 
 	// uncomment to test your merge sort function!
-	//Sorting::testSortFromCSV("large_arrays.csv", 100000);
+	Sorting::testSortFromCSV("large_arrays.csv", 100000);
 
 	// uncomment to play around with your BST!
 	Interpreter::run();
